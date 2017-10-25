d90671eaaa1a726206ac65fdec9e22c3da8b83d0
Tulio Fernandes <tulio.fernandes@fit-tecnologia.org.br>
2017-10-25 10:40:35 -0200

    Adding solution to BinaryGap, 100% score.

---
+++ BinaryGap/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int N) {
+        String binary = Integer.toBinaryString(N);
+
+        int longest = 0;
+        int count = 0;
+
+        for (char i : binary.toCharArray()) {
+            if (i == '1') {
+                if (count > longest) {
+                    longest = count;
+                }
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        
+        return longest;
+    }
+}
